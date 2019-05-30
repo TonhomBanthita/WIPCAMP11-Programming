f338a2df5e4e3e78eadae9cd4a7bc0b4324eab99
TonhomBanthita <50857698+TonhomBanthita@users.noreply.github.com>
2019-05-30 11:47:10 +0700

    [ADD]create project

---
+++ WIP-XI/src/I/Variables/Variable.java
@@ -0,0 +1,38 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package I.Variables;
+
+
+public class Variable {
+    public static void main(String[] args) {
+        double money = 150.50;
+        String name ="BANTHITA";
+        boolean x=true;
+        
+        System.out.println("Hello" +name);
+        System.out.print("money");
+        System.out.println(" วันนี้เอาเงินมา : " +money);
+        
+        System.out.println("");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int wippoWallet = 1000;
+        
+        System.out.println("wippoWallet : " + wippoWallet);
+        
+        int wippowallet = 900;
+        
+        System.out.println("wippowallet : " + wippowallet);
+        
+        
+    }
+}
